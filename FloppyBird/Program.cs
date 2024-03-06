06a186883d2c40a442ed88630baf11ab606d57c8
siva100103 <sivasuriya147@gmail.com>
2024-03-06 10:21:20 +0530

    add

--- FloppyBird/Program.cs
+++ FloppyBird/Program.cs
@@ -16,8 +16,8 @@ namespace FloppyBird
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Application.Run(new Form1());
-            Application.Run(new ToolBox());
+           Application.Run(new Form1());
+           
         }
     }
 }
